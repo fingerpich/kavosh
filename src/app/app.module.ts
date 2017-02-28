18c180acc8a08cb8f6b87141f04df2bea3131d62
mojtaba <zarei.bs@gmail.com>
2017-02-28 12:26:53 +0330

    add material-design and flex-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MaterialModule,
+    FlexLayoutModule,
     BrowserModule,
     FormsModule,
     HttpModule
